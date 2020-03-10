01ef0832bd1b2d5998013261ce32f13b28f7ede3
DunShuai Li <lidunshuai@xman.studio>
2020-03-10 21:54:48 +0800

    添加依赖，整理代码

--- src/main/java/com/matej/multidatasource/MultiDataSourceApplication.java
+++ src/main/java/com/matej/multidatasource/MultiDataSourceApplication.java
@@ -1,12 +1,8 @@
 package com.matej.multidatasource;
 
-import com.matej.multidatasource.config.DBConfig1;
-import com.matej.multidatasource.config.DBConfig2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-//@EnableConfigurationProperties(value = {DBConfig1.class, DBConfig2.class})
 @SpringBootApplication
 public class MultiDataSourceApplication {
 
